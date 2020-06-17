012d8299be3a94b961e77f9b5d1d22f6486276ac
jibaro <jessiegmatias@gmail.com>
2020-06-16 22:26:12 -0400

    Updates

---
+++ precache-manifest.88b0d460571d1c4f4f73607d6e290122.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5633c022d481980d7cf948f681c1d3",
+    "url": "/plantco-site/index.html"
+  },
+  {
+    "revision": "7eebe381d30f56fddb15",
+    "url": "/plantco-site/static/js/2.04448726.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plantco-site/static/js/2.04448726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6720eadba1ef76cd84dd",
+    "url": "/plantco-site/static/js/main.cd1faee3.chunk.js"
+  },
+  {
+    "revision": "0f046b846c4ad5fe7d97",
+    "url": "/plantco-site/static/js/runtime-main.2a0d2115.js"
+  },
+  {
+    "revision": "d3b7ab1afe19312a6bf71e6fe72cf4f2",
+    "url": "/plantco-site/static/media/arrow.d3b7ab1a.svg"
+  },
+  {
+    "revision": "2f94e647ece06e8a34c0b8d12295436c",
+    "url": "/plantco-site/static/media/firstpage1.2f94e647.jpg"
+  },
+  {
+    "revision": "3a677f6afaba053d52188fbb5a7a2173",
+    "url": "/plantco-site/static/media/firstpage2.3a677f6a.jpg"
+  },
+  {
+    "revision": "f98c4e8efd94066d0c0104189dfc3050",
+    "url": "/plantco-site/static/media/leaf.f98c4e8e.svg"
+  },
+  {
+    "revision": "dfa2296af0b90832740193328aecbee2",
+    "url": "/plantco-site/static/media/plants_beginners.dfa2296a.jpg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/plantco-site/static/media/shopping-bag.30f07071.svg"
+  }
+]);
