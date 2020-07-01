f01ba1caa2c81e827a1aac97baa5002900270065
jibaro <jessiegmatias@gmail.com>
2020-06-30 23:40:19 -0400

    fixed router issue

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render (
   <Provider store={store}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
     <PersistGate persistor={persistor} >
     <App />
     </PersistGate>
